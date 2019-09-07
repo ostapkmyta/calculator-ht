811efe78941751a860a914c47a9abb7c438bab4e
Ostap Kmyta <ostapikkmyta@gmail.com>
2019-09-07 08:36:23 +0000

    sample code

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Calculator
+    const nums = document.querySelectorAll('.calc .nums button');
+    const opers = document.querySelectorAll('.calc .opers button');
+    const display = document.querySelector('.calc .display');
+    const equal = document.querySelector('.calc .eq');
+    const erser = document.querySelector('.calc .ers')
+    nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed) );
+    function numPressed(ev) {
+        const num = ev.target.innerText;
+        display.value += num;
+    }
+    opers.forEach( buttonOper => buttonOper.addEventListener('click', operPressed) );
+    function operPressed(ev) {
+        const oper = ev.target.innerText;
+        display.value += oper;
+    }
+    equal.addEventListener('click', () => display.value = eval(display.value) );
+    erser.addEventListener('click', () => reset.value = eval(display.value) );
